58820536ac41bc8468c951334bfc4bf67b276651
Basudev Samanta <samantabasu.2001@gmail.com>
2025-08-23 17:16:25 +0000

    setup config file successfully - setup main.py complete - solve pydantic setting error

---
+++ backend/app/core/config.py
@@ -0,0 +1,16 @@
+from functools import lru_cache
+
+from pydantic_settings import BaseSettings
+from starlette.config import Config
+
+config = Config("app/.env")
+
+class Settings(BaseSettings):
+    VERSION: str = config("VERSION", default="1.0.0")
+
+@lru_cache()
+def get_settings() -> Settings:
+    settings = Settings()
+    return settings
+
+settings = get_settings()
