240a1f0c12353fc16ddad30b5360a6c253ec1877
Basudev Samanta <samantabasu.2001@gmail.com>
2025-08-30 15:00:01 +0000

    create api responses model

---
+++ backend/app/schemas/responses.py
@@ -0,0 +1,14 @@
+from typing import Generic, TypeVar, List
+
+from pydantic import BaseModel, Field
+
+T = TypeVar("T")
+
+class APIResponseModel(BaseModel):
+    message: str = Field(..., description="A message describing the result of the API call")
+    status: bool = Field(..., description="Indicates whether the API call was successful or not")
+
+class APIListResponseModel(BaseModel, Generic[T]):
+    message: str = Field(..., description="A message describing the result of the API call")
+    status: bool= Field(..., description="Indicates whether the API call was successful or not")
+    data: List[T] = Field(..., description="A list of items returned by the API call")
