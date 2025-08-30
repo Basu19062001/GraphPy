68c444c01325e7e4dab819c5dbf9f7b2748454e6
Basudev Samanta <samantabasu.2001@gmail.com>
2025-08-30 15:09:20 +0000

    signup schema ready - create signup route for the user

--- backend/app/api/v1/auth.py
+++ backend/app/api/v1/auth.py
@@ -1,13 +1,24 @@
-from fastapi import APIRouter, HTTPException, status
+from fastapi import APIRouter, HTTPException, status, Body
 from fastapi.responses import JSONResponse
 
 from app.core.db import collection
+from app.schemas.responses import APIResponseModel
+from app.schemas.auth import UserSignupModel
+from app.logger import logger
 
 router = APIRouter()
 
 users_collection = collection("users")
 
 
-@router.post("/signup",response_model= dict, summary="User signup API")
-async def signup():
-    pass
+@router.post("/signup", response_model=APIResponseModel, summary="User signup API")
+async def signup(user_payload: UserSignupModel = Body(..., description="User signup payload")):
+    try:
+        user_data = user_payload.model_dump()
+    except HTTPException as http_err:
+        logger.error(f"HTTP error during payload parsing: {http_err.detail}")
+        raise http_err
+    except Exception as e:
+        logger.error(f"Unexpected error during payload parsing: {str(e)}")
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail=f"Invalid payload. Error: {str(e)}")
+        
