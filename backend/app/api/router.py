06e69df1ed652f2fecc3fbc83f58b088450fa3b9
Basudev Samanta <samantabasu.2001@gmail.com>
2025-08-30 14:02:26 +0000

    comments unused code in the route.py file

--- backend/app/api/router.py
+++ backend/app/api/router.py
@@ -4,4 +4,4 @@ from app.api.v1 import *
 
 api_router = APIRouter()
 
-api_router.include_router(users_router, prefix="/users", tags=["Users Management APIs"])
+# api_router.include_router(users_router, prefix="/users", tags=["Users Management APIs"])
