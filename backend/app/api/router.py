31582f9943df0e0184dd415f888f0a478ef3a6ff
Basudev Samanta <samantabasu.2001@gmail.com>
2025-09-07 16:35:27 +0000

    create order.py file for the order mangement order url implement

--- backend/app/api/router.py
+++ backend/app/api/router.py
@@ -6,3 +6,4 @@ api_router = APIRouter()
 
 api_router.include_router(auth_router, prefix="/auth/user", tags=["Users Authentication APIs"])
 api_router.include_router(products_router, prefix="/products", tags=["Products Management APIs"])
+api_router.include_router(order_router, prefix="/orders", tags=["Order management APIs"])
---
+++ backend/app/api/v1/restapi/orders.py
@@ -0,0 +1,9 @@
+from fastapi import APIRouter, HTTPException, status
+
+from app.schemas.responses import APIListResponseModel, APIResponseModel
+
+router = APIRouter()
+
+@router.post("/create-orders", response_model=APIResponseModel, summary="Create order api")
+async def order_place():
+    pass
