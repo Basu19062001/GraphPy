38aeda4bf4e40ecde4477f4128955dc3a0d9541e
Basudev Samanta <samantabasu.2001@gmail.com>
2025-09-07 17:22:24 +0000

    create get_current_user method for validation route

--- backend/app/api/router.py
+++ backend/app/api/router.py
@@ -4,6 +4,6 @@ from app.api.v1 import *
 
 api_router = APIRouter()
 
-api_router.include_router(auth_router, prefix="/auth/user", tags=["Users Authentication APIs"])
+api_router.include_router(auth_router, prefix="/auth/users", tags=["Users Authentication APIs"])
 api_router.include_router(products_router, prefix="/products", tags=["Products Management APIs"])
 api_router.include_router(order_router, prefix="/orders", tags=["Order management APIs"])
--- backend/app/api/v1/restapi/orders.py
+++ backend/app/api/v1/restapi/orders.py
@@ -1,10 +1,16 @@
-from fastapi import APIRouter, HTTPException, status
+from fastapi import APIRouter, HTTPException, status, Body, Depends
 
 from app.schemas.responses import APIListResponseModel, APIResponseModel
+from app.schemas.orders import CreateOrderModel
+from app.core.db import collection
 
 router = APIRouter()
 
+orders_collection = collection("orders")
+users_collection = collection("users")
+products_collection = collection("products")
+
 
 @router.post("/create-orders", response_model=APIResponseModel, summary="Create order api")
-async def order_place():
+async def order_place(order_payload: CreateOrderModel = Body(..., description="Order creation API")):
     pass
