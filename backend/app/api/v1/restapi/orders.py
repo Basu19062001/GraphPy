73571f06ff4fe91d3c2c71a10e6457b70965b5c1
Basudev Samanta <samantabasu.2001@gmail.com>
2025-09-07 16:53:36 +0000

    order schema complete for the order creation format code by using black

--- backend/app/api/v1/restapi/orders.py
+++ backend/app/api/v1/restapi/orders.py
@@ -4,6 +4,7 @@ from app.schemas.responses import APIListResponseModel, APIResponseModel
 
 router = APIRouter()
 
+
 @router.post("/create-orders", response_model=APIResponseModel, summary="Create order api")
 async def order_place():
     pass
